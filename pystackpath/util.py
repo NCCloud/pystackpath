f272719bf5ca165aec67fb325aa279adfa38761a
Sandro Modarelli <sandro.modarelli@namecheap.com>
2019-07-03 15:10:55 +0200

    added a baseobject to interact with rest resources

--- pystackpath/util.py
+++ pystackpath/util.py
@@ -18,6 +18,25 @@ class PageInfo(object):
         self.endCursor = endCursor
 
 
+class BaseObject(object):
+    _client = None
+
+    def __init__(self, client, parent_id=0):
+        self._client = client
+        self._parent_id = parent_id
+
+    @classmethod
+    def newinstance(cls, client, parent_id=0):
+        instance = cls(client, parent_id)
+        return instance
+
+    def loaddict(self, d):
+        instance = self.newinstance(self._client, self._parent_id)
+        for key, value in d.items():
+            setattr(instance, key, value)
+        return instance
+
+
 def pagination_query(first="", after="", filter="", sort_by=""):
     params = dict()
     if first != "":
