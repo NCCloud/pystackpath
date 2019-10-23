7e8b166b02f5538bd2928842e0f5fbfc67ba6c5a
Sandro Modarelli <sandro.modarelli@namecheap.com>
2019-10-23 10:32:54 +0200

    first draft for github actions pipeline (#8)
    * first draft for github actions pipeline
    
    * dynamic pypi package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pystackpath",
-    version="0.0.2",
+    version="RELEASE_VERSION",
     author="Sandro Modarelli",
     author_email="sandro.modarelli@namecheap.com",
     description="A package to interact with StackPath",
