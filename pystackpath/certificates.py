2d885097b513bb13958f5808f6592682ac55e2e1
~dan s.~ <github@danielsteinke.com>
2019-11-12 01:36:09 -0600

    Implement get certificate [Enhancement] (#20)
    * Added "Purge" to CDNsites
    
    Implemented "Purge" endpoint to CDN Sites class.
    
    * Remobe .DS_Store
    
    Remobe .DS_Store and added it to .gitignore.
    
    * Code review changes
    
    Corrected indentation, added function description to readme.
    
    * Implement purge_status
    
    * Implemented suggested revisions.
    
    * Implement class and index.
    
    * Update stacks.py
    
    * Update stacks.py
    
    * Update certificates.py

--- pystackpath/certificates.py
+++ pystackpath/certificates.py
@@ -3,7 +3,7 @@ from .util import BaseObject, PageInfo, pagination_query
 class Certificates(BaseObject):
         def index(self, first="", after="", filter="", sort_by=""):
             pagination = pagination_query(first=first, after=after, filter=filter, sort_by=sort_by)
-            response = self._client.get("/cdn/v1/stacks/{}/certificates".format(self._parent_id), params=pagination)
+            response = self._client.get(f"/cdn/v1/stacks/{self._parent_id}/certificates", params=pagination)
             response.raise_for_status()
             items = []
             for item in response.json()["results"]:
@@ -11,3 +11,11 @@ class Certificates(BaseObject):
             pageinfo = PageInfo(**response.json()["pageInfo"])
 
             return {"results": items, "pageinfo": pageinfo}
+
+
+        def get(self, certificate_id):
+            ##https://gateway.stackpath.com/cdn/v1/stacks/{stack_id}/certificates/{certificate_id}
+            response = self._client.get(f"/cdn/v1/stacks/{self._parent_id}/certificates/{certificate_id}")
+            response.raise_for_status()
+
+            return response
