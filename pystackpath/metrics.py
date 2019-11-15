a79a501c5f156be9ae3f1b2a5bda3611c9f9de21
Sandro Modarelli <sandro.modarelli@namecheap.com>
2019-11-15 17:32:27 +0100

    improvements (#28)

--- pystackpath/metrics.py
+++ pystackpath/metrics.py
@@ -5,12 +5,17 @@ from datetime import timedelta
 
 class Metrics(BaseObject):
 
-    def get(self, start_date: dt = dt.today() - timedelta(days=1), end_date: dt = dt.today(), granularity="AUTO",
+    def get(self, start_date: dt = None, end_date: dt = None, granularity="AUTO",
             platforms=["CDE"], pops=[], billing_regions=[], sites=[], group_by="NONE", site_type_filter="ALL"):
 
         available_granularity_options = ["AUTO", "PT5M", "PT1H", "P1D", "P1M"]
         if granularity not in available_granularity_options:
             raise ValueError(f"{granularity} is not a valid granularity setting: {available_granularity_options}")
+
+        if end_date is None:
+            end_date = dt.today()
+        if start_date is None:
+            start_date = end_date - timedelta(days=1)
 
         start_date_iso = api_time_format(start_date)
         end_date_iso = api_time_format(end_date)
