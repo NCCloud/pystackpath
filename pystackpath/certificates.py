63cd7aa7e87cee78d7432401bd422441df35b797
Dario Tranchitella <dario@tranchitella.eu>
2019-11-18 09:23:58 +0100

    Update certificate (#29)
    Using the same structure we used for other resources

--- pystackpath/certificates.py
+++ pystackpath/certificates.py
@@ -1,62 +1,55 @@
 from .util import BaseObject, PageInfo, pagination_query
 
+
 class Certificates(BaseObject):
         def index(self, first="", after="", filter="", sort_by=""):
             pagination = pagination_query(first=first, after=after, filter=filter, sort_by=sort_by)
             response = self._client.get(f"/cdn/v1/stacks/{self._parent_id}/certificates", params=pagination)
             response.raise_for_status()
-            items = []
-            for item in response.json()["results"]:
-                items.append(self.loaddict(item))
+
+            items = list(map(lambda x: self.loaddict(x), response.json()["results"]))
             pageinfo = PageInfo(**response.json()["pageInfo"])
 
             return {"results": items, "pageinfo": pageinfo}
 
-
-        def get(self, certificate_id):
+        def get(self, certificate_id: str):
             response = self._client.get(f"/cdn/v1/stacks/{self._parent_id}/certificates/{certificate_id}")
             response.raise_for_status()
 
             return self.loaddict(response.json()["certificate"])
 
-
-        def add(self, certificate_string, key_string, ca_bundle_string = None):
-
+        def add(self, certificate_string: str, key_string: str, ca_bundle_string: str = None):
             data = {
                 "certificate" : certificate_string,
                 "key" : key_string,
                 "caBundle" : ca_bundle_string
             }
 
-            response = self._client.post(f"/cdn/v1/stacks/{self._parent_id}/certificates", json = data)
+            response = self._client.post(f"/cdn/v1/stacks/{self._parent_id}/certificates", json=data)
             response.raise_for_status()
 
             return self.loaddict(response.json()["certificate"])
 
-        def delete(self, certificate_id):
-
-            response = self._client.delete(f"/cdn/v1/stacks/{self._parent_id}/certificates/{certificate_id}" )
+        def delete(self):
+            response = self._client.delete(f"/cdn/v1/stacks/{self._parent_id}/certificates/{self.id}")
             response.raise_for_status()
 
             return self
 
-        def update(self, certificate_id, certificate_string = None, key_string = None, ca_bundle_string = None):
-
+        def update(self, certificate_string = None, key_string = None, ca_bundle_string: str = None):
             data = {
                 "certificate" : certificate_string,
                 "key" : key_string,
                 "caBundle" : ca_bundle_string
             }
 
-            response = self._client.put(f"/cdn/v1/stacks/{self._parent_id}/certificates/{certificate_id}",
-                json = data )
+            response = self._client.put(f"/cdn/v1/stacks/{self._parent_id}/certificates/{self.id}", json=data)
             response.raise_for_status()
 
             return self.loaddict(response.json()["certificate"])
 
-        def renew(self, certificate_id):
-
-            response = self._client.post(f"/cdn/v1/stacks/{self._parent_id}/certificates/{certificate_id}/renew")
+        def renew(self):
+            response = self._client.post(f"/cdn/v1/stacks/{self._parent_id}/certificates/{self.id}/renew")
             response.raise_for_status()
 
-            return response
+            return self
